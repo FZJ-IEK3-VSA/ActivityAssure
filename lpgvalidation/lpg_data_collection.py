37ae0340df3d58fc7572d959ecd230ac89d8ceb1
David Neuroth <d.neuroth@fz-juelich.de>
2023-05-25 13:36:42 +0200

    added some basic function signatures as an outline

---
+++ lpgvalidation/lpg_data_collection.py
@@ -0,0 +1,8 @@
+import utspclient
+
+# first version: calculate each LPG template 100 times
+# later: use extrapolation factors to determine share of each template
+
+
+def start_lpg_calculations():
+    pass
--- lpgvalidation/lpgvalidation.py
+++ lpgvalidation/lpgvalidation.py
@@ -1,1 +1,19 @@
 """Main module"""
+
+from typing import Dict
+
+import pandas as pd
+
+# TODO: alle Arbeitsschritte mit Ein- und Ausgabe definieren
+
+
+def read_lpg_profiles(path: str) -> Dict[str, pd.DataFrame]:
+    pass
+
+
+def map_templates_to_hetus_households():
+    pass
+
+
+def validation():
+    pass
