1dc23efd03e0efdf484499d7c55f343128ffe9bf
David Neuroth <d.neuroth@fz-juelich.de>
2023-06-15 16:05:52 +0200

    added docstrings and main module

--- hetus_data_processing/load_data.py
+++ hetus_data_processing/load_data.py
@@ -1,26 +1,27 @@
+"""
+Functions for loading HETUS data files.
+"""
+
 import os
 import time
-from typing import Dict, List
+from typing import Dict
 import pandas as pd
-from dataclasses import dataclass
 import logging
 
-DATAPATH = r"D:\Daten\HETUS Data\HETUS 2010 full set\DATA"
+HETUS_PATH = r"D:\Daten\HETUS Data\HETUS 2010 full set\DATA"
 HETUS_FILENAME_PREFIX = "TUS_SUF_A_"
 HETUS_FILENAME_SUFFIX = "_2010.csv"
 
-# @dataclass
-# class HetusHousehold:
-#     pass
 
-# @dataclass
-# class HetusPerson:
-#     pass
+def column_names_to_capitals(data: pd.DataFrame) -> None:
+    """
+    Changes all column headers to capitals only. Works inplace.
 
-
-# @dataclass
-# class HetusRecord:
-#     pass    
+    :param data: dataframe with headers in changing cases
+    :type data: pd.DataFrame
+    """
+    column_translations = {c: c.upper() for c in data.columns}
+    data.rename(columns=column_translations, inplace=True)
 
 
 def get_country(path: str) -> str:
@@ -36,7 +37,16 @@ def get_country(path: str) -> str:
     return name.removeprefix(HETUS_FILENAME_PREFIX).removesuffix(HETUS_FILENAME_SUFFIX)
 
 
-def get_hetus_file_names(path: str = DATAPATH) -> Dict[str, str]:
+def get_hetus_file_names(path: str = HETUS_PATH) -> Dict[str, str]:
+    """
+    Returns a dict containing all available HETUS countries and the respective
+    file paths.
+
+    :param path: path to the HETUS directory, defaults to HETUS_PATH
+    :type path: str, optional
+    :return: dict containing contry codes and file paths
+    :rtype: Dict[str, str]
+    """
     filenames = os.listdir(path)
     filenames_by_country = {}
     for name in filenames:
@@ -48,46 +58,56 @@ def get_hetus_file_names(path: str = DATAPATH) -> Dict[str, str]:
     return filenames_by_country
 
 
-def load_hetus_file(path: str) -> pd.DataFrame:
+def load_hetus_file_from_path(path: str) -> pd.DataFrame:
+    """
+    Loads a single HETUS file
+
+    :param path: the path of the file
+    :type path: str
+    :return: HETUS data from the file
+    :rtype: pd.DataFrame
+    """
+    assert os.path.isfile(path), f"File not found: {path}"
     logging.debug(f"Loading HETUS file for {get_country(path)}")
     start = time.time()
     data = pd.read_csv(path)
-    logging.info(f"Loaded HETUS file for {get_country(path)} with {len(data)} entries in {time.time() - start:.1f} s")
+    logging.info(
+        f"Loaded HETUS file for {get_country(path)} with {len(data)} entries in {time.time() - start:.1f} s"
+    )
+    column_names_to_capitals(data)
+    # assert len(data.columns) == NUM_COLUMNS, f"Unexpected number of columns: found {len(data.columns)} instead of 1939 columns"
     return data
 
 
-def load_hetus_file_for_country(country: str) -> pd.DataFrame:
+def load_hetus_file(country: str) -> pd.DataFrame:
+    """
+    Loads HETUS data of a sinlge country
+
+    :param country: the country code (e.g., "DE" for germany)
+    :type country: str
+    :raises RuntimeError: invalid country code
+    :return: HETUS data for the country
+    :rtype: pd.DataFrame
+    """
     filenames = get_hetus_file_names()
     if country.upper() not in filenames.keys():
         raise RuntimeError(f"No HETUS file for country '{country}' found")
-    return load_hetus_file(filenames[country])
+    return load_hetus_file_from_path(filenames[country])
 
 
-def load_all_hetus_files(path: str = DATAPATH) -> pd.DataFrame:
+def load_all_hetus_files(path: str = HETUS_PATH) -> pd.DataFrame:
+    """
+    Loads all available HETUS files.
+
+    :param path: the HETUS data folder, defaults to HETUS_PATH
+    :type path: str, optional
+    :return: HETUS data for all available countries
+    :rtype: pd.DataFrame
+    """
     start = time.time()
     filenames = get_hetus_file_names(path)
-    data = pd.concat(load_hetus_file(filename) for filename in filenames.values())
-    logging.info(f"Loaded all HETUS files with {len(data)} entries in {time.time() - start:.1f} s")
-    return data
-
-
-def main():
-    logging.basicConfig(
-        format="%(asctime)s %(levelname)-8s %(message)s",
-        level=logging.INFO,
-        datefmt="%Y-%m-%d %H:%M:%S",
+    data = pd.concat(load_hetus_file_from_path(filename) for filename in filenames.values())
+    logging.info(
+        f"Loaded all HETUS files with {len(data)} entries in {time.time() - start:.1f} s"
     )
-    filenames = get_hetus_file_names()
-
-    filename_germany = filenames["DE"]
-    data = load_hetus_file(filename_germany)
-    data.head()
-
-    d = load_all_hetus_files()
-    d.head()
-    print(len(d))
-
-
-
-if __name__ == "__main__":
-    main()
+    return data
---
+++ hetus_data_processing/main.py
@@ -0,0 +1,44 @@
+import logging
+import time
+from hetus_data_processing.data_analysis import compare_hh_size_and_participants
+import load_data
+import household_extraction
+import filter
+from utils import HetusColumns, HetusDayType
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    # data = load_data.load_all_hetus_files()
+    data = load_data.load_hetus_file("DE")
+    #TODO: remove inconsistent households also from full data set
+    hhdata = household_extraction.get_household_data(data)
+    print(f"Number of diary entries: {len(data)}")
+    print(f"Number of households: {len(hhdata)}")
+
+    compare_hh_size_and_participants(data)
+
+    filters = {
+        HetusColumns.Diary.WEEKDAY: [1],
+        HetusColumns.Diary.MONTH: [6, 7, 8],
+        HetusColumns.General.COUNTRY: ["DE", "AT"],
+        HetusColumns.HH.SIZE: [1,2,3,4],
+    }
+    d = filter.filter_combined(data, filters)
+    print(len(d))
+
+
+    pass
+
+
+    #TODO: check which percentage of the data has missing values in the relevant fields (filter fields and diaries)
