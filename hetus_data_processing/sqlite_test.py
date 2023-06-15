294d93545ce0dcc66cb5deb75f606bccc503185f
David Neuroth <d.neuroth@fz-juelich.de>
2023-06-15 16:07:01 +0200

    added script to test conversion to sqlite DB

---
+++ hetus_data_processing/sqlite_test.py
@@ -0,0 +1,29 @@
+"""
+Testing conversion of the HETUS data into an sqlite database and working with that.
+"""
+
+import hetus_data_processing.load_data as load_data
+
+import sqlite3
+import sqlalchemy
+
+
+def setup_table():
+    con = sqlite3.connect("hetus.db")
+    cur = con.cursor()
+
+    data = load_data.load_hetus_file("DE")
+
+    colums = ",".join(data.columns)
+    cur.execute(f"CREATE TABLE if not exists hetus({colums})")
+
+    d = list(data.itertuples(index=False))
+    paramstring = ",".join("?" * len(d[0]))
+    cur.executemany(f"insert into hetus values({paramstring})", d)
+    con.commit()
+
+
+con = sqlite3.connect("hetus.db")
+cur = con.cursor()
+
+# cur.execute("SELECT TOP 1 HHC1 FROM hetus GROUPBY COUNTRY, HID ")
