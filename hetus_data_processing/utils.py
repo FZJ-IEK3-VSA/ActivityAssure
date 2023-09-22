54560c9e7f8db47ed0296289e39ce14ce79973eb
David Neuroth <d.neuroth@fz-juelich.de>
2023-09-22 17:30:55 +0200

    added common function to save result files

--- hetus_data_processing/utils.py
+++ hetus_data_processing/utils.py
@@ -1,6 +1,64 @@
+import errno
 import logging
+import os
 from functools import wraps
 from time import time
+from typing import Any, Iterable, List
+
+import pandas as pd
+
+#: path for result data # TODO: move to config file
+DATA_PATH = "./data"
+
+
+def ensure_dir_exists(path: str) -> None:
+    """
+    Ensures that the specified path exists. If not, creates
+    the path including all subdirectories.
+
+    :param path: the path to check or create
+    :type path: str
+    """
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
+def save_file(
+    data: pd.DataFrame,
+    subdir: str | List[str],
+    filename: str,
+    category: Iterable,
+    ext: str = "csv",
+) -> None:
+    """
+    Saves a result file within the main data directory
+
+    :param data: data to save
+    :type data: pd.DataFrame
+    :param subdir: subdirectory to save the file at
+    :type subdir: str | List[str]
+    :param filename: name of the file
+    :type filename: str
+    :param category: data category, if applicable
+    :type category: Any
+    :param ext: file extension, defaults to "csv"
+    :type ext: str, optional
+    """
+    if category is not None:
+        # add category to filename
+        filename = f"{filename}_{'_'.join(str(c) for c in category)}"
+    filename += f".{ext}"
+    # turn subdir into a list if it is not already one
+    subdir = subdir if isinstance(subdir, List) else [subdir]
+    directory = os.path.join(DATA_PATH, *subdir)
+    ensure_dir_exists(directory)
+
+    path = os.path.join(directory, filename)
+    data.to_csv(path)
+    logging.debug(f"Created file {directory}")
 
 
 def timing(f):
