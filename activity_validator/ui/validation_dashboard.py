d8cb6d10d4ed01c6d45374639f536c84d2eb10d2
David Neuroth <d.neuroth@fz-juelich.de>
2023-10-26 12:18:38 +0200

    first attempt of activity frequency sum plots

--- activity_validator/ui/validation_dashboard.py
+++ activity_validator/ui/validation_dashboard.py
@@ -1,11 +1,18 @@
 import dataclasses
 from datetime import datetime, timedelta
+import glob
 from pathlib import Path
 from dash import Dash, html, dcc, callback, Output, Input
 import dash_bootstrap_components as dbc
+import plotly.express as px
 import pandas as pd
 from activity_validator.hetus_data_processing import activity_profile
 from activity_validator.hetus_data_processing.activity_profile import ProfileType
+from activity_validator.hetus_data_processing.attributes.diary_attributes import DayType
+from activity_validator.hetus_data_processing.attributes.person_attributes import (
+    Sex,
+    WorkStatus,
+)
 from activity_validator.ui.overview import chunks, create_rows, draw_figure
 
 from activity_validator.ui.probability_curves import (
@@ -16,6 +23,7 @@ from activity_validator.ui.probability_curves import (
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # default data paths
+# validation_path = Path("data/validation_data")
 validation_path = Path("data/validation_data EU mapped")
 input_data_path = Path("data/lpg/results")
 
@@ -28,11 +36,44 @@ metrics_dir = "metrics"
 diff_dir = "differences"
 
 
+def load_data_by_type(path, profile_type):
+    filter = str(path) + "/*" + profile_type.construct_filename() + ".csv"
+
+    # find the correct file
+    files = glob.glob(filter)
+    if len(files) == 0:
+        raise RuntimeError(f"Could not find a matching file: {filter}")
+    if len(files) > 1:
+        raise RuntimeError(f"Found multiple files for the same profile type: {files}")
+    # load the correct file
+    _, data = activity_profile.load_df(files[0])
+    return data
+
+
+def draw_activity_figure(subdir: str, profile_type: ProfileType):
+    # profile_type = ProfileType.from_iterable(value.split(" - "))
+    dv = load_data_by_type(validation_path / subdir, profile_type)
+    di = load_data_by_type(input_data_path / subdir, profile_type)
+    di.rename(columns={c: c + " - LPG" for c in di.columns}, inplace=True)
+
+    data_sets = []
+    for col in dv.columns:
+        c2 = col + " - LPG"
+        if not c2 in di.columns:
+            continue
+        d = pd.concat([dv[col], di[c2]], axis=1)
+        data_sets.append(d)
+    return [dcc.Graph(figure=px.ecdf(d)) for d in data_sets]
+
+
 # get available validation profile types
 profile_types = get_profile_types(validation_path / prob_dir)
 countries = list({p.country for p in profile_types})
 global_profile_types = {dataclasses.replace(p, country="") for p in profile_types}
 global_type_str = [" - ".join(pt.to_tuple()[1:]) for pt in profile_types]
+
+# TODO just for testing
+test_profile_type = ProfileType("DE", Sex.female, WorkStatus.full_time, DayType.no_work)
 
 tab1_content = html.Div(
     [
@@ -52,6 +93,12 @@ tab1_content = html.Div(
             [
                 html.H2("Difference", style={"textAlign": "center"}),
                 AIOSelectableProbabilityCurves(input_data_path / comp_dir / diff_dir),
+            ]
+        ),
+        html.Div(
+            [
+                html.H2("Activity Frequencies", style={"textAlign": "center"}),
+                html.Div(draw_activity_figure(freq_dir, test_profile_type)),
             ]
         ),
     ]
@@ -95,7 +142,6 @@ app.layout = html.Div(
                 dcc.Tab(tab2_content, label="Single Country Overview"),
                 dcc.Tab(tab3_content, label="Cross-country Overview"),
             ],
-            # value=tab1_content,
         ),
     ]
 )
