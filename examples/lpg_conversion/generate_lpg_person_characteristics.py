bfa8c8b2d671835badba226db885cb38c58930c1
David Neuroth <d.neuroth@fz-juelich.de>
2023-12-16 17:50:57 +0100

    automatically generate LPG person characteristics

--- examples/lpg_conversion/generate_lpg_person_characteristics.py
+++ examples/lpg_conversion/generate_lpg_person_characteristics.py
@@ -14,21 +14,73 @@ from activity_validator.hetus_data_processing.attributes.person_attributes impor
     WorkStatus,
 )
 
+# Define mappings for the LPG database fields
+GENDER_MAPPING = {0: Sex.male, 1: Sex.female}
+WORK_STATUS_MAPPING = {
+    # "Living Pattern / All": None,  # no person has this tag
+    "Living Pattern / Kindergarden": WorkStatus.student,
+    "Living Pattern / Maid / Day Maid": WorkStatus.full_time,
+    "Living Pattern / Office Job": WorkStatus.full_time,
+    "Living Pattern / Office Job / Early (5-7am)": WorkStatus.full_time,
+    "Living Pattern / Office Job / Late (9-11am)": WorkStatus.full_time,
+    "Living Pattern / Office Job / Medium (7-9am)": WorkStatus.full_time,
+    "Living Pattern / Office Worker": WorkStatus.full_time,
+    "Living Pattern / Part Time Job": WorkStatus.part_time,
+    "Living Pattern / Retiree": WorkStatus.retired,
+    "Living Pattern / School": WorkStatus.student,
+    "Living Pattern / School / Medium (7-9am)": WorkStatus.student,
+    "Living Pattern / Shift work": WorkStatus.full_time,
+    "Living Pattern / Shift work / 3 Shifts A": WorkStatus.full_time,
+    "Living Pattern / Shift work / 3 Shifts B": WorkStatus.full_time,
+    "Living Pattern / Stay at Home": WorkStatus.unemployed,
+    "Living Pattern / Stay at Home / Drifting": WorkStatus.unemployed,
+    "Living Pattern / Stay at Home / Regular": WorkStatus.unemployed,
+    "Living Pattern / Two Shift Work": WorkStatus.full_time,
+    "Living Pattern / University": WorkStatus.student,
+    "Living Pattern / University / Student Independent": WorkStatus.student,
+    "Living Pattern / University / Student Living at Home": WorkStatus.student,
+    "Living Pattern / Work From Home": WorkStatus.full_time,
+    "Living Pattern / Work From Home / Full Time 5 days": WorkStatus.full_time,
+    "Living Pattern / Work From Home / Part Time": WorkStatus.part_time,
+}
 
-def define_person_mapping(path: Path):
-    # TODO: (automatically?) create complete mapping for all persons/households
+
+def person_name(full_name: str) -> str:
+    """
+    Extracts the actual person name out of the
+    full name string, e.g.: 'CHR01 Rubi' -> 'Rubi'
+
+    :param full_name: the full name incl. household id
+    :return: the short name
+    """
+    return full_name.split(" ")[1]
+
+
+def define_person_mapping(database_path: Path, result_path: Path):
+    assert database_path.is_file(), f"Input database of LPG is missing: {database_path}"
+    # query the person information, including name, gender and the living pattern tag each
+    # person has in his/her household
+    con = sqlite3.connect(database_path)
+    cur = con.cursor()
+    query = """select tblPersons.Name, tblPersons.Gender1, tblLivingPatternTags.Name
+from tblPersons inner join tblCHHPersons on tblPersons.ID == tblCHHPersons.PersonID inner join tblLivingPatternTags on tblCHHPersons.LivingPatternTagID == tblLivingPatternTags.ID"""
+    results = cur.execute(query)
+    rows: list[tuple] = results.fetchall()
+
+    # map all fields
     mapping = {
-        "Rubi": activity_profile.ProfileType("DE", Sex.female, WorkStatus.full_time),
-        "Sami": activity_profile.ProfileType("DE", Sex.male, WorkStatus.full_time),
-        "Cordelia": activity_profile.ProfileType("DE", Sex.female, WorkStatus.retired),
-        "Edgar": activity_profile.ProfileType("DE", Sex.male, WorkStatus.retired),
+        person_name(person): activity_profile.ProfileType(
+            "DE", GENDER_MAPPING[gender], WORK_STATUS_MAPPING[tag]
+        )
+        for person, gender, tag in rows
     }
-    dict_mapping = {n: p.to_dict() for n, p in mapping.items()}  # type: ignore
 
-    path.parent.mkdir(parents=True, exist_ok=True)
-    with open(path, "w+", encoding="utf-8") as f:
+    # write all characteristics to a json file
+    dict_mapping = {n: p.to_dict() for n, p in mapping.items()}  # type: ignore
+    result_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(result_path, "w+", encoding="utf-8") as f:
         json.dump(dict_mapping, f, indent=4)
-    print(f"Generated person characteristics file: {path}")
+    print(f"Generated person characteristics file: {result_path}")
 
 
 def generate_raw_activity_mapping(database_path: str, output_file):
@@ -73,9 +125,9 @@ def generate_raw_activity_mapping(database_path: str, output_file):
 
 
 if __name__ == "__main__":
+    lpg_main_db = Path("data/lpg/profilegenerator.db3")
     person_file = Path("data/lpg/person_characteristics.json")
-    define_person_mapping(person_file)
+    define_person_mapping(lpg_main_db, person_file)
 
-    lpg_main_db = "data/lpg/profilegenerator.db3"
-    output_file = "examples/activity_mapping_lpg.json"
-    generate_raw_activity_mapping(lpg_main_db, output_file)
+    # output_file = "examples/activity_mapping_lpg.json"
+    # generate_raw_activity_mapping(lpg_main_db, output_file)
