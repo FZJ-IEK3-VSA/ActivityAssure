ba905f639e9d971a865353e26b6fc5785b7a5e50
David Neuroth <d.neuroth@fz-juelich.de>
2023-12-18 13:06:26 +0100

    deleted obsolete modules

--- activity_validator/lpgvalidation/lpg_data_collection.py
+++
@@ -1,8 +0,0 @@
-import utspclient
-
-# first version: calculate each LPG template 100 times
-# later: use extrapolation factors to determine share of each template
-
-
-def start_lpg_calculations():
-    pass
