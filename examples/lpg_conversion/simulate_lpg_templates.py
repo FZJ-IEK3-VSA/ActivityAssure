b927953a770d0e541fe750abcbc8fafc3cdcba3f
David Neuroth <d.neuroth@fz-juelich.de>
2024-01-09 16:37:14 +0100

    fixed lpg mass simulation script to tolerate errors

--- examples/lpg_conversion/simulate_lpg_templates.py
+++ examples/lpg_conversion/simulate_lpg_templates.py
@@ -33,43 +33,55 @@ print(f"UTSP-Server: {address}")
 result_file = "Results.HH1.sqlite"
 templates = [HouseholdTemplates.CHR01_Couple_both_at_Work]
 templates = [v for k, v in vars(HouseholdTemplates).items() if not k.startswith("__")]
-repetitions_per_hh = 1  # 100
+repetitions_per_hh = 1  # 50
 
-# TODO: check which result files are already there, and only request the missing files
-all_requests = []
-all_templates = []
+template_guids_and_requests = []
 for template in templates:
-    print(f"--- Sending requests for template {template} ---")
     hhdata = utspclient.helpers.lpg_helper.create_hhdata_from_template(template)
     simulation_config.House.Households = [hhdata]
-    requests = [
-        TimeSeriesRequest(
-            simulation_config.to_json(),  # type: ignore
-            "LPG",
-            guid=f"{i}",
-            required_result_files=dict.fromkeys([result_file]),
+    template_id = template.split(" ")[0]
+    # create simulation ID and request object for each run and store as
+    # list of tuples
+    new_ids_and_requests = [
+        (
+            template_id,
+            i,
+            TimeSeriesRequest(
+                simulation_config.to_json(),  # type: ignore
+                "LPG",
+                guid=f"{i}",
+                required_result_files=dict.fromkeys([result_file]),
+            ),
         )
         for i in range(repetitions_per_hh)
     ]
-    # TODO: temporary solution
-    all_requests.extend(requests)
-    all_templates.extend([template] * len(requests))
+    template_guids_and_requests.extend(new_ids_and_requests)
 
-requests = all_requests
-
+template_names, guids, requests = zip(*template_guids_and_requests)
 start = time.time()
-results = utspclient.calculate_multiple_requests(REQUEST_URL, requests, API_KEY)
+results = utspclient.calculate_multiple_requests(
+    REQUEST_URL, requests, API_KEY, raise_exceptions=False
+)
 d = round(time.time() - start, 2)
-print(f"Calculation of {len(requests)} requests took {d} sec.")
+print(f"Calculation of {len(new_ids_and_requests)} requests took {d} sec.")
 
 # save all result files
 base_result_path = Path("data/lpg/raw")
-for template, request, result in zip(all_templates, requests, results):
-    assert not isinstance(result, Exception)
-    file_content = result.data[result_file]
-    short_name = template.split(" ")[0]
-    result_path = base_result_path / short_name
-    result_path.mkdir(parents=True, exist_ok=True)
-    filename = result_path / f"{short_name}_{request.guid}.sqlite"
-    with open(filename, "wb") as f:
-        f.write(file_content)
+errors_path = base_result_path / "errors"
+errors_path.mkdir(parents=True, exist_ok=True)
+for template, guid, request, result in zip(template_names, guids, requests, results):
+    filename = f"{template}_{request.guid}.sqlite"
+    # check if the calculation failed
+    if isinstance(result, Exception):
+        # write the exception to file in a dedicated errors directory
+        filepath = errors_path / f"{template}_{request.guid}_error.txt"
+        with open(filepath, "w") as f:
+            f.write(str(result))
+    else:
+        # write the results to file
+        result_path = base_result_path / template
+        result_path.mkdir(parents=True, exist_ok=True)
+        file_content = result.data[result_file]
+        filepath = result_path / filename
+        with open(filepath, "wb") as f:
+            f.write(file_content)
