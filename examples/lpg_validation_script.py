060b0e3d362b958a591f925568e4de65a0d26a8f
David Neuroth <d.neuroth@fz-juelich.de>
2024-02-09 17:42:23 +0100

    extenden lpg result analysis script

--- examples/lpg_validation_script.py
+++ examples/lpg_validation_script.py
@@ -6,12 +6,23 @@ the best and worst person categories.
 import pandas as pd
 from pathlib import Path
 
-result_path = Path("data/lpg/results/metrics/metrics_per_category.csv")
-
+base_path = "data/lpg/results_cluster/metrics/metrics_per_category_"
 mean_idx = "mean"
 
-data = pd.read_csv(result_path)
+data = pd.read_csv(base_path + "default.csv")
+data_scaled = pd.read_csv(base_path + "scaled.csv")
+data_scaled = data_scaled.loc[:, ("mae", "rmse", "wasserstein")].add_prefix("scaled_")  # type: ignore
+data = pd.concat([data, data_scaled], axis=1)
 
-means = data[data.iloc[:, 1] == mean_idx]
-means = means.sort_values(by="mae")
-print(means)
+data["diff"] = data["scaled_mae"] - data["mae"]
+data.dropna(inplace=True)
+print(data.describe())
+
+filtered = data
+# filter category
+# filtered = data[data.iloc[:, 0] == "DE_female_full time_work"]
+# filter activity
+# filtered = data[data.iloc[:, 1] == mean_idx]
+
+filtered = filtered.sort_values(by="bias")
+print(filtered)
