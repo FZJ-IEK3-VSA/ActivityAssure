67e31e56a49b0823c9d416104e420293fb284691
David Neuroth <d.neuroth@fz-juelich.de>
2024-02-26 17:30:44 +0100

    check product of mae, rmse, wasserstein in validation script

--- examples/lpg_validation_script.py
+++ examples/lpg_validation_script.py
@@ -20,15 +20,19 @@ data_scaled = data_scaled.loc[:, ("mae", "rmse", "bias", "wasserstein")].add_pre
 data = pd.concat([data, data_scaled], axis=1)
 
 data["diff_mae"] = data["scaled_mae"] - data["mae"]
+data["prod"] = data.loc[:, ["mae", "rmse", "wasserstein"]].product(axis=1) * 10**6
 print(data.describe())
 
 # data.dropna(inplace=True)
 
 data = data
 # filter category
-# data = data[data.iloc[:, 0] == "DE_female_retired_no work"]
+# data = data[
+#     (data.iloc[:, 0] == "DE_female_student_working day")
+#     | (data.iloc[:, 0] == "DE_male_student_working day")
+# ]
 # filter activity
 data = data[data.iloc[:, 1] == mean_idx]
 
-data = data.sort_values(by="mae")
+data = data.sort_values(by="prod")
 print(data)
