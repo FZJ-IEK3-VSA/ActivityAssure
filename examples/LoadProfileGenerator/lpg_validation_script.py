c1fcbfc184aeb1a25e83aeec1c8bd462384fdc61
David Neuroth <d.neuroth@fz-juelich.de>
2024-03-02 17:01:38 +0100

    updated validation script

--- examples/LoadProfileGenerator/lpg_validation_script.py
+++ examples/LoadProfileGenerator/lpg_validation_script.py
@@ -10,9 +10,8 @@ from activity_validator.comparison_indicators import ValidationIndicators
 # TODO: turn this into a function to show a quick overview at the end of the validate_lpg() function?
 #      E.g. overall best and worst categories/activities, worst according to each indicator etc.
 
-base_path = (
-    "data/lpg/results_tmp/validation_results/default/indicators_per_category.csv"
-)
+result_dir = "per_person"
+base_path = f"data/lpg_validation/{result_dir}/validation_results/default/indicators_per_category.csv"
 mean_idx = ValidationIndicators.mean_column
 
 data = pd.read_csv(base_path)
@@ -21,19 +20,19 @@ data_scaled = data_scaled.loc[:, ("mae", "rmse", "bias", "wasserstein")].add_pre
 data = pd.concat([data, data_scaled], axis=1)
 
 data["diff_mae"] = data["scaled_mae"] - data["mae"]
-data["prod"] = data.loc[:, ["mae", "rmse", "wasserstein"]].product(axis=1) * 10**6
+data["prod"] = data.loc[:, ["mae", "rmse", "wasserstein"]].product(axis=1) * 10**3
 print(data.describe())
 
 # data.dropna(inplace=True)
 
 data = data
+data = data[data.iloc[:, 1] == mean_idx]
 # filter category
 # data = data[
 #     (data.iloc[:, 0] == "DE_female_student_working day")
 #     | (data.iloc[:, 0] == "DE_male_student_working day")
 # ]
 # filter activity
-data = data[data.iloc[:, 1] == mean_idx]
 
 data = data.sort_values(by="prod")
 print(data)
