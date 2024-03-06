118ef25d7358d70d562dbf5c9b91c30271cdf513
David Neuroth <d.neuroth@fz-juelich.de>
2024-03-06 17:39:37 +0100

    updated lpg_validation_script

--- examples/LoadProfileGenerator/lpg_validation_script.py
+++ examples/LoadProfileGenerator/lpg_validation_script.py
@@ -11,33 +11,32 @@ from activity_validator.comparison_indicators import ValidationIndicators
 #      E.g. overall best and worst categories/activities, worst according to each indicator etc.
 
 result_dir = "per_person"
-base_path = f"data/lpg_validation/{result_dir}/validation_results/default/indicators_per_category.csv"
+base_path = "data/lpg_validation/{}/validation_results/{}/indicators_per_category.csv"
 mean_idx = ValidationIndicators.mean_column
 
-data = pd.read_csv(base_path)
-data_scaled = pd.read_csv(base_path.replace("default", "scaled"))
+data = pd.read_csv(base_path.format(result_dir, "default"))
+data_scaled = pd.read_csv(base_path.format(result_dir, "scaled"))
+# add the scaled indicators to the default dataframe
 data_scaled = data_scaled.loc[:, ["mae", "rmse", "bias", "wasserstein"]].add_prefix(
     "scaled_"
 )
 data = pd.concat([data, data_scaled], axis=1)
 
+# check various composite indicators
 data["diff_mae"] = data["scaled_mae"] - data["mae"]
 data["prod"] = data.loc[:, ["mae", "rmse", "wasserstein"]].product(axis=1) * 10**3
-print(data.describe())
+# print(data.describe())
 
 # data.dropna(inplace=True)
 
 data = data[data.iloc[:, 1] == mean_idx]
 # filter category
-# data = data[
-#     (data.iloc[:, 0] == "DE_female_student_working day")
-#     | (data.iloc[:, 0] == "DE_male_student_working day")
-# ]
-# filter activity
+data = data[(data.iloc[:, 0].str.startswith("DE_male_unemployed_rest day"))]
 
 data = data.sort_values(by="prod")
 
-print("--- Best")
-print(data[:20])
-print("\n--- Worst")
-print(data[-20:])
+print(data)
+# print("\n--- Best")
+# print(data[:20])
+# print("\n--- Worst")
+# print(data[-20:])
