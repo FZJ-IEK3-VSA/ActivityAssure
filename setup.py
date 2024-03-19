24c02280209dd21585cb873378d3def27e41604d
David Neuroth <d.neuroth@fz-juelich.de>
2024-03-19 11:43:47 +0100

    added dist and build to gitignore

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(dir_path, "README.md"), "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="activity_validator",
+    name="activity-validator",
     version="0.1.0",
     author="David Neuroth",
     author_email="d.neuroth@fz-juelich.de",
