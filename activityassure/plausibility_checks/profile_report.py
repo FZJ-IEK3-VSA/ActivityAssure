4b67ba3a0c346ef61bddfecd3525aab4b89888b9
David Neuroth <d.neuroth@fz-juelich.de>
2025-03-27 22:39:17 +0100

    adapted activity profile plausibility report objects

--- activityassure/plausibility_checks/profile_report.py
+++ activityassure/plausibility_checks/profile_report.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
 from dataclasses import dataclass, field
-from enum import StrEnum
+
+from activityassure.activity_profile import SparseActivityProfile
 
 
 @dataclass(frozen=True)
@@ -35,9 +37,10 @@ class FailedTestResult(TestResult):
 
 
 @dataclass
-class ProfileReport:
+class ResultCollection:
     errors: list[TestResult] = field(default_factory=list)
     successes: list[TestResult] = field(default_factory=list)
+    description: str = ""
 
     def add(self, entry: TestResult):
         if entry.ok:
@@ -45,17 +48,45 @@ class ProfileReport:
         else:
             self.errors.append(entry)
 
-    def get_failed_ratio(self) -> float:
+    def get_fail_rate(self) -> float:
         return len(self.errors) / (len(self.errors) + len(self.successes))
 
     def get_errors(self) -> list[TestResult]:
         return self.errors
 
+    def get_results(self) -> list[TestResult]:
+        return self.successes + self.errors
+
     def get_str_report(self, include_successful: bool = True):
-        report = ""
+        report = f"Test results for {self.description}:\n"
         if include_successful:
             for e in self.successes:
                 report += e.get_message() + "\n"
         for e in self.errors:
             report += e.get_message() + "\n"
         return report
+
+
+@dataclass
+class PlausibilityReport:
+    profiles: list[SparseActivityProfile] = field(default_factory=list)
+    reports: list[ResultCollection] = field(default_factory=list)
+
+    def add_report(self, profile: SparseActivityProfile, report: ResultCollection):
+        self.profiles.append(profile)
+        self.reports.append(report)
+
+    def get_results_by_check(self) -> dict[str, ResultCollection]:
+        results: dict[str, ResultCollection] = defaultdict(ResultCollection)
+        for report in self.reports:
+            for result in report.get_results():
+                results[result.check_id].add(result)
+        for check, collection in results.items():
+            collection.description = f"check '{check}'"
+        return results
+
+    def get_fail_rate_by_check(self) -> dict[str, float]:
+        results = self.get_results_by_check()
+        return {
+            check: collection.get_fail_rate() for check, collection in results.items()
+        }
