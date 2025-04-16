bdb4f0b3bb04a040ec03f199b3bcb34e7516c093
David Neuroth <d.neuroth@fz-juelich.de>
2025-04-16 17:23:36 +0200

    improved sleep checks

--- activityassure/plausibility_checks/sleep_checks.py
+++ activityassure/plausibility_checks/sleep_checks.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from datetime import timedelta
+from typing import Iterable
 from activityassure.activity_profile import SparseActivityProfile
 from activityassure.plausibility_checks.profile_report import (
     FailedTestResult,
@@ -14,9 +15,19 @@ class SleepChecks:
     MAX_AWAKE_TIME = timedelta(hours=24)
     MIN_SLEEP_SHARE = 0.2
 
-    def __init__(self, profile: SparseActivityProfile, report: ResultCollection):
+    def __init__(
+        self,
+        profile: SparseActivityProfile,
+        report: ResultCollection,
+        sleep_name: str = "sleep",
+        ignore: Iterable[str] = [],
+    ):
+        self.sleep_act = sleep_name
         self.profile = profile
-        self.sleep_activities = [a for a in profile.activities if a.name == "sleep"]
+        self.ignore = set(ignore)
+        self.sleep_activities = [
+            a for a in profile.activities if a.name == self.sleep_act
+        ]
         self.report = report
 
     def check_sleep(self) -> None:
@@ -27,73 +38,89 @@ class SleepChecks:
             self.report.add(result)
             # abort: other tests cannot be concluded in a meaningful way
             return
-        self.check_overall_sleep_ratio()
-        self.check_sleep_durations()
-        self.check_awake_durations()
+        # conduct all sleep checks
+        results = [
+            self.check_overall_sleep_ratio(),
+            self.check_sleep_durations(),
+            self.check_awake_durations(),
+        ]
+        for result in results:
+            self.report.add(result)
 
-    def check_overall_sleep_ratio(self) -> None:
+    def check_overall_sleep_ratio(self) -> TestResult:
+        check = "sleep - overall ratio"
         sleep_duration = sum(a.duration for a in self.sleep_activities)
         sleep_share = sleep_duration / self.profile.length()
         if sleep_share < SleepChecks.MIN_SLEEP_SHARE:
-            result = FailedTestResult(
-                "sleep - overall ratio",
+            return FailedTestResult(
+                check,
                 f"Overall sleeping time is too low at {sleep_share * 100:.1f}%",
             )
-            self.report.add(result)
+        else:
+            return SuccessfulTest(check)
 
-    def check_sleep_durations(self) -> None:
-        too_long_sleeps = [
-            a
-            for a in self.sleep_activities
-            if a.duration * self.profile.resolution > SleepChecks.MAX_SLEEP_TIME
-        ]
+    def check_sleep_durations(self) -> TestResult:
         check = "sleep - duration"
-        if too_long_sleeps:
-            result: TestResult = FailedTestResult(
+        too_long_sleep_durs = [
+            duration
+            for a in self.sleep_activities
+            if (duration := a.duration * self.profile.resolution)
+            > SleepChecks.MAX_SLEEP_TIME
+        ]
+        if too_long_sleep_durs:
+            return FailedTestResult(
                 check,
-                f"Person slept for longer than {SleepChecks.MAX_SLEEP_TIME}.",
-                len(too_long_sleeps),
-                len(too_long_sleeps) / len(self.sleep_activities),
+                f"Person slept for longer than {SleepChecks.MAX_SLEEP_TIME} (max.: {max(too_long_sleep_durs)})",
+                len(too_long_sleep_durs),
+                len(too_long_sleep_durs) / len(self.sleep_activities),
             )
         else:
-            result = SuccessfulTest(check)
-        self.report.add(result)
+            return SuccessfulTest(check)
 
-    def check_awake_durations(self) -> None:
-        wake_time: defaultdict[str, int] = defaultdict(int)
-        max_awake_timesteps = SleepChecks.MAX_AWAKE_TIME / self.profile.resolution
-        too_long_awake = 0
+    def check_awake_durations(self) -> TestResult:
+        check = "sleep - time awake"
+        wake_time: defaultdict[str, list[int]] = defaultdict(list)
+        too_long_awake = []
         last_sleep_idx = -1
         for i, a in enumerate(self.profile.activities):
-            if a.name == "sleep":
+            if a.name == self.sleep_act:
                 # check the duration of this awake period
-                if (
+                time_awake = (
                     a.start - self.profile.activities[last_sleep_idx].end()
-                    > max_awake_timesteps
-                ):
-                    too_long_awake += 1
+                ) * self.profile.resolution
+                if time_awake > SleepChecks.MAX_AWAKE_TIME:
+                    # check if one of the activities to ignore occurred
+                    ignore_this_occasion = False
+                    for awake_act in self.profile.activities[last_sleep_idx + 1 : i]:
+                        if awake_act.name in self.ignore:
+                            ignore_this_occasion = True
+                            break
+                    if ignore_this_occasion:
+                        # this awake period was caused by an activity like vacation that is ignored
+                        continue
+
                     # person was awake for too long, check responsible activitites
+                    too_long_awake.append(time_awake)
                     for wake_activity in self.profile.activities[
                         last_sleep_idx + 1 : i
                     ]:
-                        wake_time[wake_activity.name] += wake_activity.duration
+                        wake_time[wake_activity.name].append(wake_activity.duration)
                 last_sleep_idx = i
-                continue
         # sort by activity duration
         wake_time_str = "\n".join(
-            f"\t{k}: {v * self.profile.resolution}"
+            f"\t{k}: {sum(v) * self.profile.resolution} total, {sum(v) * self.profile.resolution / len(v)}"
+            f" average, {max(v) * self.profile.resolution} max"
             for k, v in sorted(
                 wake_time.items(), key=lambda pair: pair[1], reverse=True
             )
         )
-        if too_long_awake == 0:
-            self.report.add(SuccessfulTest("sleep - time awake"))
-            return
-        result = FailedTestResult(
-            "sleep - time awake",
-            f"Person was awake for longer than {SleepChecks.MAX_AWAKE_TIME}",
-            too_long_awake,
-            too_long_awake / len(self.sleep_activities),
+        if len(too_long_awake) == 0:
+            return SuccessfulTest(check)
+        max_awake = max(too_long_awake)
+        return FailedTestResult(
+            check,
+            f"Person was awake for longer than {SleepChecks.MAX_AWAKE_TIME} (max.: {max_awake})",
+            len(too_long_awake),
+            len(too_long_awake) / len(self.sleep_activities),
             f"Responsible activities:\n{wake_time_str}",
         )
-        self.report.add(result)
