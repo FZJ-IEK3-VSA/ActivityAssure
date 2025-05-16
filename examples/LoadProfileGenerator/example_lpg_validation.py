634f358b92d67646706c2fe2e81a7f09f78595dd
David Neuroth <d.neuroth@fz-juelich.de>
2025-05-16 16:46:41 +0200

    moved function with per category validation routine to validation module; cut the optional all-combinations part from it

--- examples/LoadProfileGenerator/example_lpg_validation.py
+++ examples/LoadProfileGenerator/example_lpg_validation.py
@@ -7,62 +7,8 @@ from datetime import timedelta
 import logging
 from pathlib import Path
 
-from activityassure import utils, validation
 from activityassure.input_data_processing import process_model_data
-from activityassure.visualizations import indicator_heatmaps
-from activityassure.validation_statistics import ValidationSet
-
-
-@utils.timing
-def validate(
-    input_path: Path, validation_path: Path, compare_all_combinations: bool = False
-):
-    """
-    Load input and validation statistics and compare them
-    using indicators and heatmaps.
-
-    :param compare_all_combinations: if True, in attition to the normal
-                                     per-category validation, all combinations
-                                     of profile categories will be checked;
-                                     defaults to False
-    """
-    # load LPG statistics and validation statistics
-    input_statistics = ValidationSet.load(input_path)
-    validation_statistics = ValidationSet.load(validation_path)
-
-    # compare input and validation data statistics per profile category
-    indicator_set_variants = validation.validate_per_category(
-        input_statistics, validation_statistics, input_path
-    )
-    validation_result_path = input_path / "validation_results"
-
-    # save indicators and heatmaps for each indicator variant
-    for variant_name, indicator_set in indicator_set_variants.items():
-        result_subdir = validation_result_path / variant_name
-        metrics_df = indicator_set.save(result_subdir / "indicators_per_category")
-
-        # plot heatmaps to compare indicator values
-        plot_path = result_subdir / "heatmaps"
-        indicator_heatmaps.plot_indicators_by_profile_type(metrics_df, plot_path)
-        indicator_heatmaps.plot_indicators_by_activity(metrics_df, plot_path)
-        indicator_heatmaps.plot_profile_type_by_activity(metrics_df, plot_path)
-
-    if compare_all_combinations:
-        # compare statistics for each combination of profile categories
-        indicators_all_combinations = validation.validate_all_combinations(
-            input_statistics, validation_statistics
-        )
-        validation.save_file_per_indicator_per_combination(
-            indicators_all_combinations, input_path
-        )
-
-        # plot heatmaps to compare the different categories to each other
-        indicator_heatmaps.plot_category_comparison(
-            indicators_all_combinations, plot_path
-        )
-        indicator_heatmaps.plot_category_comparison_per_activity(
-            indicators_all_combinations, plot_path
-        )
+from activityassure import validation
 
 
 if __name__ == "__main__":
@@ -108,4 +54,4 @@ if __name__ == "__main__":
     input_statistics.save(input_stats_path)
 
     # validate the input data using the statistics
-    validate(input_stats_path, validation_stats_path_merged)
+    validation.default_validation(input_stats_path, validation_stats_path_merged)
