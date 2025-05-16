539c7f7c82c46d44137e5547768877a35c77226c
David Neuroth <d.neuroth@fz-juelich.de>
2025-05-16 20:24:12 +0200

    fixed and improved load profile processing

--- examples/CitySimulation/load_profile_processing.py
+++ examples/CitySimulation/load_profile_processing.py
@@ -2,7 +2,10 @@
 sum profiles for the whole city"""
 
 from datetime import datetime, timedelta
+import gc
+import itertools
 from pathlib import Path
+from typing import Iterable
 
 import psutil
 import pandas as pd
@@ -20,23 +23,47 @@ def aggregate_load_sum_profiles(city_result_dir: Path, output_dir: Path):
     houses_subdir = city_result_dir / "Houses"
     house_dirs = list(houses_subdir.iterdir())
     house_num = len(house_dirs)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    result_file_path = output_dir / "City.SumProfiles.Electricity.csv"
     print(f"Aggregating load profiles from {house_num} houses.")
 
-    # parse the sum profiles from all houses
-    all_profiles = []
+    # parse first file completely, including time stamps
+    data = pd.read_csv(
+        house_dirs[0] / filename,
+        sep=";",
+        index_col=False,
+        parse_dates=["Time"],
+        date_format=dateformat,
+        dtype={0: int, data_col_name: float},
+    )
+    data.rename(columns={data_col_name: house_dirs[0].name}, inplace=True)
+
+    # parse the sum profiles from all other houses
+    collected_profiles = []
     last_update = datetime.now()
-    for i, house_dir in enumerate(house_dirs):
+    for i, house_dir in enumerate(house_dirs[1:]):
         profile_path = house_dir / filename
         profile = pd.read_csv(
-            profile_path,
-            sep=";",
-            index_col=0,
-            parse_dates=["Time"],
-            date_format=dateformat,
-            dtype={0: int, data_col_name: float},
+            profile_path, sep=";", index_col=False, usecols=[data_col_name]
         )
         profile.rename(columns={data_col_name: house_dir.name}, inplace=True)
-        all_profiles.append(profile)
+        collected_profiles.append(profile)
+
+        # regularly combine all collected dataframes
+        if i % 500 == 0 or i == house_num - 1:
+            if data is not None:
+                # add the alread concatenated profiles
+                all_profiles: Iterable[pd.DataFrame] = itertools.chain(
+                    [data], collected_profiles
+                )
+            else:
+                all_profiles = collected_profiles
+
+            # concatenate the dataframes
+            data = pd.concat(all_profiles, axis="columns")
+            # reset the list to release the individual dataframes
+            collected_profiles = []
+            gc.collect()
 
         # print progress
         if datetime.now() - last_update > timedelta(seconds=30):
@@ -46,16 +73,14 @@ def aggregate_load_sum_profiles(city_result_dir: Path, output_dir: Path):
                 flush=True,
             )
 
-    data = pd.concat(all_profiles, axis="columns")
-
-    output_dir.mkdir(parents=True, exist_ok=True)
-    data.to_csv(output_dir / "City.SumProfiles.Electricity.csv")
+    assert data is not None
+    data.to_csv(result_file_path)
 
 
 if __name__ == "__main__":
     city_result_dir = Path(
         "/fast/home/d-neuroth/city_simulation_results/scenario_city-julich_25"
     )
-    # city_result_dir = Path("D:/LPG/Results/scenario_julich-grosse-rurstr_fullroutes")
+    # city_result_dir = Path("D:/LPG/Results/test")
     output_dir = Path(f"data/city/postprocessed/{city_result_dir.name}")
     aggregate_load_sum_profiles(city_result_dir, output_dir)
