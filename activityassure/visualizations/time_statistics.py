11f92fba59d9410d9dde2e811d6c5548a5f55e6e
David Neuroth <d.neuroth@fz-juelich.de>
2025-05-21 10:31:50 +0200

    sort activities in bar chart by share in national dataset

--- activityassure/visualizations/time_statistics.py
+++ activityassure/visualizations/time_statistics.py
@@ -13,7 +13,6 @@ def category_to_plot_label(category: ProfileCategory) -> str:
     return category_parts[1] + '_' + category_parts[0]
 
 
-
 def plot_total_time_spent(
         statistics_country_1: dict[ProfileCategory, ValidationStatistics],
         statistics_country_2: dict[ProfileCategory, ValidationStatistics],
@@ -23,16 +22,21 @@ def plot_total_time_spent(
     for _, v in (statistics_country_1 | statistics_country_2 | national_statistics).items():
         time_activity_distribution[v.profile_type] = v.probability_profiles.mean(axis=1) * 24
 
+    # get the column name of the first country
+    nat1 = next(iter(national_statistics.keys()))
+
     fig, ax = plt.subplots(figsize=(16*CM_TO_INCH, 16*CM_TO_INCH))
     combined_df = pd.DataFrame(time_activity_distribution)
-    sorted_cols = sorted(combined_df.columns, key=category_to_plot_label)  # type: ignore
-    combined_df[sorted_cols].T.plot(kind='barh', stacked=True, ax=ax)
+
+    # sort by activity share in first country
+    sorted_df = combined_df.sort_values(nat1, ascending=False)
+    sorted_cols = sorted(sorted_df.columns, key=category_to_plot_label)  # type: ignore
+    sorted_df[sorted_cols].T.plot(kind='barh', stacked=True, ax=ax)
 
     ax.legend(loc='lower right', bbox_to_anchor=(1, 1), ncol=3)    
     ax.set_xlim(0, 24)
     fig.tight_layout()
     fig.savefig("test.png") # TODO: save in proper location
 
-    # TODO: plot total time spent using national dataset (in separate plot?)
     # TODO: combine small shares
     # TODO: sort by share so that largest shares are on the left
