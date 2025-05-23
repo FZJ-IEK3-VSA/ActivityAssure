323b930094e7bc8970bc1610fc44374ef3db28be
David Neuroth <d.neuroth@fz-juelich.de>
2025-05-23 12:55:44 +0200

    extracted function creating activity plots into a new module

---
+++ examples/CitySimulation/activity_plots.py
@@ -0,0 +1,31 @@
+from activityassure.profile_category import ProfileCategory
+from activityassure.ui import data_utils, datapaths, plots
+
+
+from pathlib import Path
+
+
+def create_selected_activity_assure_plots():
+    citysim_path = Path("data/city/validation/scenario_city-julich_25_merged")
+    citysim_national = Path(f"{citysim_path}_national")
+    # validation_national = Path(
+    #     "data/validation_data_sets/activity_validation_data_set_national"
+    # )
+
+    # input_statistics = ValidationSet.load(citysim_path)
+
+    plot_dir = Path("data/diss_validation_plots")
+
+    profile_type = ProfileCategory("DE")
+    filepath = data_utils.get_file_path(
+        citysim_national / datapaths.prob_dir, profile_type
+    )
+    fig = plots.stacked_prob_curves(filepath)
+    assert fig
+    data_utils.save_plot(
+        fig,
+        "probability profiles",
+        name="De",
+        profile_type=profile_type,
+        base_path=plot_dir,
+    )
--- examples/CitySimulation/city_simulation_analysis.py
+++ examples/CitySimulation/city_simulation_analysis.py
@@ -13,8 +13,6 @@ import activity_statistics_validation
 import load_profile_analysis
 import poi_validation
 import calc_statistics
-from activityassure.profile_category import ProfileCategory
-from activityassure.ui import data_utils, datapaths, plots
 
 
 def population_statistics(path: Path, result_dir: Path):
@@ -34,32 +32,6 @@ def population_statistics(path: Path, result_dir: Path):
     sns.barplot(x="measure", ax=ax, y="Value", hue="Dataset", data=df_long)
     fig.savefig(result_dir / "population_statistics_comparison.svg")
     plt.show()
-
-
-def create_selected_activity_assure_plots():
-    citysim_path = Path("data/city/validation/scenario_city-julich_25_merged")
-    citysim_national = Path(f"{citysim_path}_national")
-    # validation_national = Path(
-    #     "data/validation_data_sets/activity_validation_data_set_national"
-    # )
-
-    # input_statistics = ValidationSet.load(citysim_path)
-
-    plot_dir = Path("data/diss_validation_plots")
-
-    profile_type = ProfileCategory("DE")
-    filepath = data_utils.get_file_path(
-        citysim_national / datapaths.prob_dir, profile_type
-    )
-    fig = plots.stacked_prob_curves(filepath)
-    assert fig
-    data_utils.save_plot(
-        fig,
-        "probability profiles",
-        name="De",
-        profile_type=profile_type,
-        base_path=plot_dir,
-    )
 
 
 def main():
