35caf32825b980d033d4f00a6125934116a7df9e
David Neuroth <d.neuroth@fz-juelich.de>
2025-06-03 11:14:47 +0200

    always include activity 'other' in 'minor activities' in stacked bar plot

--- activityassure/visualizations/time_statistics.py
+++ activityassure/visualizations/time_statistics.py
@@ -11,38 +11,43 @@ def category_to_plot_label(category: ProfileCategory) -> str:
     category_parts = str(category).split("_", 1)
     if len(category_parts) == 1:
         return category_parts[0]
-    return category_parts[1] + '_' + category_parts[0]
+    return category_parts[1] + "_" + category_parts[0]
 
 
 def plot_total_time_spent(
-        statistics_country_1: dict[ProfileCategory, ValidationStatistics],
-        statistics_country_2: dict[ProfileCategory, ValidationStatistics],
-        national_statistics: dict[ProfileCategory, ValidationStatistics],
-        plot_path: Path,
-    ):
+    statistics_country_1: dict[ProfileCategory, ValidationStatistics],
+    statistics_country_2: dict[ProfileCategory, ValidationStatistics],
+    national_statistics: dict[ProfileCategory, ValidationStatistics],
+    plot_path: Path,
+):
     time_activity_distribution = {}
-    for _, v in (statistics_country_1 | statistics_country_2 | national_statistics).items():
-        time_activity_distribution[v.profile_type] = v.probability_profiles.mean(axis=1) * 24
+    for _, v in (
+        statistics_country_1 | statistics_country_2 | national_statistics
+    ).items():
+        time_activity_distribution[v.profile_type] = (
+            v.probability_profiles.mean(axis=1) * 24
+        )
 
     # get the column name of the first country
     nat1 = next(iter(national_statistics.keys()))
 
-    fig, ax = plt.subplots(figsize=(16*CM_TO_INCH, 16*CM_TO_INCH))
+    fig, ax = plt.subplots(figsize=(16 * CM_TO_INCH, 16 * CM_TO_INCH))
     combined_df = pd.DataFrame(time_activity_distribution)
 
     # sort by activity share in first country
     sorted_df = combined_df.sort_values(nat1, ascending=False)  # type: ignore
-    
-    # combine all activities with a low overall share
-    min_share = 0.05 * 24
-    summed = sorted_df[sorted_df[nat1] < min_share].sum()
-    summed.name = "minor activities"
-    
-    sorted_df = pd.concat([sorted_df[sorted_df[nat1] >= min_share], summed.to_frame().T])
-    sorted_cols = sorted(sorted_df.columns, key=category_to_plot_label)  # type: ignore
-    sorted_df[sorted_cols].T.plot(kind='barh', stacked=True, ax=ax)
 
-    ax.legend(loc='lower right', bbox_to_anchor=(1, 1), ncol=3)    
+    # combine all activities with a low overall share and include the activity "other"
+    min_share = 0.05 * 24
+    condition = (sorted_df[nat1] < min_share) | (sorted_df.index == "other")
+    summed = sorted_df[condition].sum()
+    summed.name = "minor activities"
+
+    sorted_df = pd.concat([sorted_df[~condition], summed.to_frame().T])
+    sorted_cols = sorted(sorted_df.columns, key=category_to_plot_label)  # type: ignore
+    sorted_df[sorted_cols].T.plot(kind="barh", stacked=True, ax=ax)
+
+    ax.legend(loc="lower right", bbox_to_anchor=(1, 1), ncol=3)
     ax.set_xlim(0, 24)
     fig.tight_layout()
     fig.savefig(plot_path / "time_spent.png")
