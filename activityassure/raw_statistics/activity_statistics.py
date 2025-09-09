156a92423c31ff5cb1886d12f0433244936c7573
David Neuroth <d.neuroth@fz-juelich.de>
2025-09-09 13:40:16 +0200

    added more raw activity statistics

--- activityassure/raw_statistics/activity_statistics.py
+++ activityassure/raw_statistics/activity_statistics.py
@@ -13,14 +13,40 @@ from activityassure.activity_profile import SparseActivityProfile
 
 
 def sort_by_val(d: dict) -> dict:
+    """Sorts a dict by value in descending order.
+
+    :param d: the dict to sort
+    :return: a new, sorted dict
+    """
     return dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
+
+
+def save_dict(filepath: Path, data: dict) -> None:
+    """Saves a dict to a json file
+
+    :param filepath: target file path
+    :param data: the dict to save
+    """
+    with open(filepath, "w", encoding="utf8") as f:
+        json.dump(data, f, indent=4)
+
+
+def dur_dict_to_strs(d: dict[str, int], resolution: timedelta) -> dict[str, str]:
+    """Converts a duration dict with int values to a dict containing
+    timedelta strings.
+
+    :param d: the dict with duration values in timesteps
+    :param resolution: the resolution to apply
+    :return: the resulting dict with timedelta strings
+    """
+    return {k: str(v * resolution) for k, v in d.items()}
 
 
 def get_activity_statistics(
     profile_dir: Path, output_dir: Path, resolution=timedelta(minutes=1)
 ):
-    total_occurrences = Counter()
-    total_durations = defaultdict(int)
+    occurrences = Counter()
+    durations = defaultdict(int)
     user_counts = defaultdict(int)
     files = list(profile_dir.iterdir())
     for csv_file in tqdm(files):
@@ -29,41 +55,47 @@ def get_activity_statistics(
         activity_profile = SparseActivityProfile.load_from_csv(
             csv_file, None, resolution  # type: ignore
         )
-        # add occurrences, durations and
+        # add occurrences, durations and user counts
         names = [a.name for a in activity_profile.activities]
-        total_occurrences.update(names)
+        occurrences.update(names)
         for activity in set(names):
             user_counts[activity] += 1
         for activity in activity_profile.activities:
-            total_durations[activity.name] += activity.duration
+            durations[activity.name] += activity.duration
 
     # get user shares relative to total person count
     person_count = len(files)
     user_shares = {k: v / person_count for k, v in user_counts.items()}
 
     # convert durations to timedelta strings
-    total_durations = sort_by_val(total_durations)
-    total_dur_strs = {k: str(v * resolution) for k, v in total_durations.items()}
+    durations = sort_by_val(durations)
+    total_dur_strs = dur_dict_to_strs(durations, resolution)
+
+    # get the average total duration per person that carries out the activity
+    dur_per_user = {k: v / person_count / user_shares[k] for k, v in durations.items()}
+    dur_per_user = sort_by_val(dur_per_user)
+    dur_per_user_strs = dur_dict_to_strs(dur_per_user, resolution)
+
+    # get the average number of occurences per person that carries out the activity
+    occ_per_user = sort_by_val(
+        {k: v / person_count / user_shares[k] for k, v in occurrences.items()}
+    )
 
     # collect general statistics
     stats = {
         "persons": person_count,
-        "total profile duration": str(
-            resolution * sum(v for v in total_durations.values())
-        ),
+        "total profile duration": str(resolution * sum(v for v in durations.values())),
     }
 
-    # save statistics
+    # save all statistics in json files
     output_dir.mkdir(parents=True, exist_ok=True)
     print(f"Calculated general activity statistics from {len(files)} profiles")
-    with open(output_dir / "general_info.json", "w", encoding="utf8") as f:
-        json.dump(stats, f, indent=4)
-    with open(output_dir / "total_occurrences.json", "w", encoding="utf8") as f:
-        json.dump(dict(total_occurrences.most_common()), f, indent=4)
-    with open(output_dir / "user_shares.json", "w", encoding="utf8") as f:
-        json.dump(sort_by_val(user_shares), f, indent=4)
-    with open(output_dir / "total_durations.json", "w", encoding="utf8") as f:
-        json.dump(total_dur_strs, f, indent=4)
+    save_dict(output_dir / "general_info.json", stats)
+    save_dict(output_dir / "total_occurrences.json", dict(occurrences.most_common()))
+    save_dict(output_dir / "user_shares.json", sort_by_val(user_shares))
+    save_dict(output_dir / "total_durations.json", total_dur_strs)
+    save_dict(output_dir / "duration_per_user.json", dur_per_user_strs)
+    save_dict(output_dir / "occurrences_per_user.json", occ_per_user)
 
 
 if __name__ == "__main__":
