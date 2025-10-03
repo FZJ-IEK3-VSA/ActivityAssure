06383f2cd48103726e9eb55cb0679dc2fa2eb37b
David Neuroth <d.neuroth@fz-juelich.de>
2025-10-03 12:15:47 +0200

    fixed plot paths, added log messages for peak load in sum curves

--- examples/CitySimulation/comparisons.py
+++ examples/CitySimulation/comparisons.py
@@ -1,5 +1,6 @@
 """Comparisons and assessments across multiple result data sets"""
 
+import logging
 from pathlib import Path
 
 from matplotlib import pyplot as plt
@@ -41,6 +42,13 @@ def charging_power_comparison_sumprofiles(base_results: Path, output: Path):
     ax.yaxis.set_label_text(f"Elektrische Last [{unit3}]")
     fig.savefig(output / "sum_duration.svg")
 
+    max3 = float(data3kW[DFColumnsLoad.TOTAL_LOAD].max())
+    max11 = float(data11kW[DFColumnsLoad.TOTAL_LOAD].max())
+    logging.info(
+        f"Sum curve maximums:\n3 kW: {max3} {unit3}\n11 kW: "
+        f"{max11} {unit3}\nDifference: {max11 - max3} {unit3}"
+    )
+
 
 def charging_power_comparison_maxloads(base_results: Path, output: Path):
     subdir = f"{SubDirs.POSTPROCESSED_DIR}/loads/aggregated_household"
@@ -71,8 +79,8 @@ def charging_power_comparison_total_demand(base_results: Path, output: Path):
     subdir = f"{SubDirs.POSTPROCESSED_DIR}/loads/aggregated_household"
     path3kW = base_results / "scenario_julich_100_3kW" / subdir
     path11kW = base_results / "scenario_julich_100_11kW" / subdir
-    data3kW = pd.read_csv(path3kW / LoadFiles.TOTALS, parse_dates=[0])
-    data11kW = pd.read_csv(path11kW / LoadFiles.TOTALS, parse_dates=[0])
+    data3kW = pd.read_csv(path3kW / LoadFiles.TOTALS)
+    data11kW = pd.read_csv(path11kW / LoadFiles.TOTALS)
 
     data3kW.sort_values(DFColumnsLoad.TOTAL_DEMAND, inplace=True, ascending=False)
     data11kW.sort_values(DFColumnsLoad.TOTAL_DEMAND, inplace=True, ascending=False)
@@ -102,15 +110,16 @@ def charging_power_comparison_total_demand(base_results: Path, output: Path):
     ax.xaxis.set_label_text("Haushalte")
     ax.yaxis.set_label_text("Stromverbrauch [kWh]")
     ax2.yaxis.set_label_text("Durchschnittliche Last [W]")
-    fig.savefig(output / "peak_duration.svg")
+    fig.savefig(output / "total_demands.svg")
 
 
 def charging_power_comparison(base_results: Path, output: Path):
+    logging.info("--- charging power scenarios (3 kW vs. 11 kW) ---")
     charge_output = output / "charging_power_3kW_vs_11kW"
     charge_output.mkdir(parents=True, exist_ok=True)
-    charging_power_comparison_sumprofiles(base_results, output)
-    charging_power_comparison_maxloads(base_results, output)
-    charging_power_comparison_total_demand(base_results, output)
+    charging_power_comparison_sumprofiles(base_results, charge_output)
+    charging_power_comparison_maxloads(base_results, charge_output)
+    charging_power_comparison_total_demand(base_results, charge_output)
 
 
 def main():
@@ -118,8 +127,15 @@ def main():
     base_results = Path("R:/phd_dir/results")
     output = base_results / "comparisons"
     output.mkdir(parents=True, exist_ok=True)
+    sns.set_theme()  # default theme
     charging_power_comparison(base_results, output)
 
 
 if __name__ == "__main__":
+    # init logging
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
     main()
